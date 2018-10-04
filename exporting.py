ef7c62a18d1ca4d2e530f855682f7a7356e55e3a
Luis Esquivel <lalejandro.0925@gmail.com>
2018-10-04 16:42:21 -0600

    - Adding export to csv functionality

--- exporting.py
+++ exporting.py
@@ -1,9 +1,10 @@
-# def export_soup(self,
-#                 file_name="Processed_Soup",
-#                 folder="C:/Users/Ale/Documents/Python Projects/Crawling/Sandbox_Folder"):
-#     file_name_format = file_name + '.csv'
-#     file_path = '/'.join([folder, file_name_format])
-#     print(file_path)
-#     with open(file_path, 'w+', encoding="utf-8", newline='') as f:
-#         writer = csv.writer(f)
-#         writer.writerows(self.relational_soup)
+import csv
+
+
+def export_soup(file_name, matrix_content, folder="ouput_box"):
+    file_name_format = file_name + '.csv'
+    file_path = '/'.join([folder, file_name_format])
+    print(file_path)
+    with open(file_path, 'w+', encoding="utf-8", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(matrix_content)
